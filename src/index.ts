7e7323348825316195478d745cea8c197c501500
Jay <jay.sanjay.karia@gmail.com>
2024-12-01 22:43:04 +0530

    feat: implement user input for limit

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import { INITIAL_FRACTIONS, INITIAL_NUM } from "./constants"
 import { greaterThanOne, updateFractions, printFractions } from "./lib"
 import { FractionType, NumType } from "./types/index"
+import PromptSync = require("prompt-sync")
+
+const prompt = PromptSync()
 
 let num: NumType = INITIAL_NUM
 let fractions: FractionType[] = INITIAL_FRACTIONS
@@ -9,7 +12,9 @@ let numerators = []
 let denominators = []
 
 let fractionIndex: 0 | 1 | 2 = 0
-let currentFractionIndex = 0
+let totalFractions = 0
+
+const limit = prompt("Enter the limit (Skip to see all outcomes): ")
 
 do {
   const currentNumerator = fractions[fractionIndex].numerator
@@ -22,7 +27,7 @@ do {
     updateFractions({ num, fractions, currentNumerator, currentDenominator })
     fractionIndex = 1
   } else fractionIndex++
-  currentFractionIndex++
-} while (!greaterThanOne(fractions, fractionIndex))
+  totalFractions++
+} while (limit ? totalFractions < parseInt(limit) : !greaterThanOne(fractions, fractionIndex))
 
 printFractions({ numerators, denominators })
